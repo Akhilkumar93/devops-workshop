ab712c7145ac7b3c4139932f51d2b83ded432a73
tsri010203@gmail.com <tsri>
2024-03-30 10:46:10 +0530

    adding terraform_code

---
+++ terrafrom_code/v1_ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1" 
+    access_key = "AKIAZQ3DSDPSGSQUQL6C"
+  secret_key = "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"
+}
+
+resource "aws_instance" "demo-server"{
+    ami = "ami-080e1f13689e07408"
+    instance_type ="t2.medium"
+    key_name = "dpp"
+}
